a89af4b780537ab8e9d393cac660aa87075d6ad9
Gerald Gorman <gpgorman158@gmail.com>
2022-02-09 01:17:48 -0500

    commit tree 1a41215c1b58f09409be37a17c53ffcf66d8d009

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
-  
+
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        console.log(event);
+        const inputValue = event.target.searchByID.value;
+        const inputValue2 = event.target.querySelector('#searchByID').value;
+        const inputValue3 = event.target.children[1].value;
+        const input = document.querySelector('input#searchByID').value;
+        console.log(inputValue +" "+  inputValue2 + " " + inputValue3  + " " + input);
+
+        fetch(`http://localhost:3000/movies/${input}`)
+        .then(function (response){
+            return response.json();})   
+        .then(function (json) {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            title.textContent = json.title;
+            summary.innerText = json.summary;
+
+        })
+    });
+    
 }
 
 document.addEventListener('DOMContentLoaded', init);
